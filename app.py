152e3522eaf65fbf5a27b18485b8f9a534c61816
PUTTAGUNTA DHANUNJAY <147498414+Dhanunjay7777@users.noreply.github.com>
2025-02-15 10:09:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS  
 from deep_translator import GoogleTranslator
+from translater import app  # Ensure the filename matches
 
 app = Flask(__name__)
 CORS(app)  
